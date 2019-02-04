241a496095ac826cc24ce9eb4e4888d5247674de
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2019-02-04 12:55:50 +0300

    initialize

---
+++ vue22-component-arasi-iliskiler-eventBus/src/components/User.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <h1>Parent Component (User)</h1>
+    <p>Burası parent component yani herşeyin import edildiği component :)</p>
+    <button @click="changeName">Adımı Değiştir</button>
+    <p>Clid componentten gelen : {{ childData }}</p>
+    <p>Kullanıcı yaşı : {{age}}</p>
+    <hr>
+    <div class="row">
+      <app-user-detail :age="age" :name="title" @data='childData=$event'></app-user-detail>
+      <app-user-edit :age="age" @ageWasEdited="age= $event"></app-user-edit>
+    </div>
+  </div>
+</template>
+<script>
+import UserDetail from "./UserDetail";
+import UserEdit from "./UserEdit";
+export default {
+  components: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit
+  },
+  data: function() {
+    return {
+      title: "User componentinden gelen",
+      childData: "",
+      age: 25
+    };
+  },
+  methods: {
+    changeName: function() {
+      this.title = "İçerik buton aracılığı ile değiştirildi.";
+    }
+  }
+};
+</script>
+
+<style>
+div.container {
+  margin-top: 30px;
+  padding: 20px 40px;
+  background-color: #6a8d99;
+  border: 1px solid #666;
+}
+div.row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
---
+++ vue22-component-arasi-iliskiler-eventBus/src/components/UserEdit.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col-md-s6">
+    <h3>Child Component 2</h3>
+    <p>Ben User.vue isimli Parent Component'in içerisindeki bir diğer Child componentim</p>
+    <p>Kullanıcı Yaşı : {{ age }}</p>
+    <button @click="changeAge">Yaş bilgisini Değiştir.</button>
+  </div>
+</template>
+<script>
+export default {
+  props: ["age"],
+  methods: {
+    changeAge: function() {
+      this.age = 30;
+      this.$emit('ageWasEdited', this.age);
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightgoldenrodyellow;
+  padding: 20px;
+  border: 1px solid #666;
+  display: inline-block;
+}
+</style>
+
