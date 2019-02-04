b4fb154976da389dc742cb8d7376fa4172e2cba7
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2019-02-04 12:54:09 +0300

    componentler arası veri aktarımı parent üzerinden

--- vue22-component-arasi-iliskiler/src/components/UserDetail.vue
+++ vue22-component-arasi-iliskiler/src/components/UserDetail.vue
@@ -2,9 +2,32 @@
   <div class="wcol-md-6">
     <h3>Child Component 1</h3>
     <p>Ben User.vue isimli Parent Component'in içerisindeki bir Child componentim</p>
+    <p>Kullanıcı Adı : {{ name }}</p>
+    <p>Kullanıcı Adı : {{ switchName() }}</p>
+    <p>Kullanıcı Yaşı : {{age}}</p>
+    <button @click="sendToParent">Child componentten root yada parenta gönder.</button>
   </div>
 </template>
-<script></script>
+<script>
+  export default {
+    props: {
+      name: {
+        type: String,
+        // required: true,
+        default: 'Burası default değeri.'
+      },
+      age: [Number]
+    },
+    methods: {
+      switchName() {
+        return this.name.split('').reverse().join('');
+      },
+      sendToParent: function() {
+        this.$emit('data','child componentten gelen.');
+      }
+    }
+  }
+</script>
 <style scoped>
   div {
     background-color: lightcoral;
